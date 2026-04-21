46aa3e66705c2cf0f32053a036ca48a51fd2fda3
chop0005 <cnevitt0005@gmail.com>
2026-04-21 09:34:05 -0500

    Add ventures/__init__.py

---
+++ ventures/__init__.py
@@ -0,0 +1,8 @@
+from .base import VentureSpec, VentureType, VENTURE_TYPES, rank_by_capital_and_speed
+from .digital_product import (
+    research_opportunity,
+    generate_product_content,
+    generate_listing_pack,
+    generate_product_batch,
+    generate_launch_strategy
+)
