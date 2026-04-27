2c7d437c0d11bd501123208555b42f5c3b73e647
chop0005 <cnevitt0005@gmail.com>
2026-04-27 10:25:51 -0500

    Add agents/__init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,3 +1,4 @@
 from .opportunity_scanner import run_opportunity_scanner, scan_for_opportunities, OpportunityApproveView, opportunity_embed
 from .build_pipeline import run_build_pipeline
 from .revenue_tracker import run_revenue_tracker, revenue_embed
+from .ai_chat import run_ai_chat_monitor, handle_chat_message, model_status_embed
