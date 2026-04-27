8866a5870646eae115239531f57b7fac292b6104
chop0005 <cnevitt0005@gmail.com>
2026-04-27 08:15:56 -0500

    Add ventures/__init__.py

--- ventures/__init__.py
+++ ventures/__init__.py
@@ -1,8 +1,9 @@
 from .base import VentureSpec, VentureType, VENTURE_TYPES, rank_by_capital_and_speed
 from .digital_product import (
-    research_opportunity,
-    generate_product_content,
-    generate_listing_pack,
-    generate_product_batch,
-    generate_launch_strategy
+    research_opportunity, generate_listing_pack,
+    generate_product_batch, generate_launch_strategy
 )
+from .product_generator import generate_product, decide_product_type
+from .pdf_generator import generate_pdf_product
+from .notion_generator import generate_notion_template
+from .etsy_manager import EtsyClient, EtsyApprovalView, listing_approval_embed
