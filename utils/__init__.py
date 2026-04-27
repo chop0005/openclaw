3958416d2267e3a51c73543a49cf17bb492f34d5
chop0005 <cnevitt0005@gmail.com>
2026-04-27 10:52:01 -0500

    v4.2 fix: utils/__init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,1 @@
-from .claude import (
-    think, think_json, think_code, think_code_json,
-    route, pick_model, get_available_models, MODELS, build_system_prompt
-)
+from .claude import think, think_json, think_code, think_code_json
