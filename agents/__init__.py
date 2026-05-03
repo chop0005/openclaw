4fd19795b29a04a00ab5d812e6319768365a759d
chop0005 <cnevitt0005@gmail.com>
2026-05-03 06:10:47 -0500

    v5.1: agents/__init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,5 +2,3 @@ from .opportunity_scanner import run_opportunity_scanner, scan_for_opportunities
 from .build_pipeline import run_build_pipeline
 from .revenue_tracker import run_revenue_tracker, revenue_embed
 from .ai_chat import run_ai_chat_monitor, handle_chat_message, model_status_embed
-from .analytics import run_analytics_agent, analyze_etsy_market, analyze_own_performance, analytics_embed
-from .newsletter import run_newsletter_agent, generate_lead_magnet, lead_magnet_embed, NEWSLETTER_SETUP
