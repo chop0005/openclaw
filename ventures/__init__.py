898b1673c2a6bbf596337438eea16ac1b74007f5
chop0005 <cnevitt0005@gmail.com>
2026-05-03 06:11:05 -0500

    v5.1: ventures/__init__.py

--- ventures/__init__.py
+++ ventures/__init__.py
@@ -1,11 +1,2 @@
-from .base import VentureSpec, VentureType, VENTURE_TYPES, rank_by_capital_and_speed
-from .digital_product import (
-    research_opportunity, generate_listing_pack,
-    generate_product_batch, generate_launch_strategy
-)
-from .product_generator import generate_product, decide_product_type
-from .pdf_generator import generate_pdf_product
-from .notion_generator import generate_notion_template
-from .etsy_manager import EtsyClient, EtsyApprovalView, listing_approval_embed, ETSY_SETUP_GUIDE
-from .gumroad_manager import GumroadClient, GumroadApprovalView, GUMROAD_SETUP
-from .pinterest_manager import PinterestClient, auto_pin_product, pinterest_embed, PINTEREST_SETUP
+# Ventures are imported lazily inside functions to prevent startup crashes
+# Each venture module is imported only when needed
